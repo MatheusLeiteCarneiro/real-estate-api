b22569e8e1023f575694076930f554785d5e40a6
Matheus Leite Carneiro <matheus.leite.carneiro@gmail.com>
2026-03-25 23:36:10 -0300

    intial commit

---
+++ src/test/java/com/mlcdev/realestate/RealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlcdev.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
