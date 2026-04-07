e6a463b47929032f0a990db942029acd95366924
Matheus Leite Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-06 23:35:10 -0300

    refactor: add custom ZipCode and State deserializer to format those fields

---
+++ src/main/java/com/mlcdev/realestate/util/StateDeserializer.java
@@ -0,0 +1,20 @@
+package com.mlcdev.realestate.util;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+
+public class StateDeserializer extends JsonDeserializer<String> {
+
+    @Override
+    public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        String value = jsonParser.getText();
+        if(value == null){
+            return null;
+        }
+        return value.trim().toUpperCase();
+    }
+}
