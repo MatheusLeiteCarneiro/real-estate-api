41c4eae38e7bd42b93f942959ab9b6d4c3130158
Matheus Leite Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-08 23:29:02 -0300

    feat: create custom query to find the primary image of a property

--- src/main/java/com/mlcdev/realestate/repository/ImageRepository.java
+++ src/main/java/com/mlcdev/realestate/repository/ImageRepository.java
@@ -2,6 +2,7 @@ package com.mlcdev.realestate.repository;
 
 import com.mlcdev.realestate.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +10,7 @@ import java.util.UUID;
 public interface ImageRepository extends JpaRepository<Image, UUID> {
 
     List<Image> findAllByPropertyId(UUID propertyId);
+
+    @Query("SELECT img FROM Image img WHERE img.property.id = :propertyId AND img.isPrimary = true")
+    Image findPropertyPrimaryImage(UUID propertyId);
 }
