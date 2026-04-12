bb7fb7f2a141fb25353fa527b631f828eee48934
Matheus Leite Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-12 12:41:00 -0300

    fix: add @Repository annotation to repositories

--- src/main/java/com/mlcdev/realestate/repository/ImageRepository.java
+++ src/main/java/com/mlcdev/realestate/repository/ImageRepository.java
@@ -2,11 +2,13 @@ package com.mlcdev.realestate.repository;
 
 import com.mlcdev.realestate.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, UUID> {
 
     List<Image> findAllByPropertyId(UUID propertyId);
--- src/main/java/com/mlcdev/realestate/repository/UserRepository.java
+++ src/main/java/com/mlcdev/realestate/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.mlcdev.realestate.repository;
 import com.mlcdev.realestate.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
