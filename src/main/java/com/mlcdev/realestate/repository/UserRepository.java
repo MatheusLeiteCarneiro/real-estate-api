9a6981a23eacb2b8613290ae8ec75244af8f10d3
Matheus Leite Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-12 12:31:23 -0300

    feat: create user repository and method to find user by username

---
+++ src/main/java/com/mlcdev/realestate/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mlcdev.realestate.repository;
+
+import com.mlcdev.realestate.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+}
