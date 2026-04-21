350bb6beb0b7caf672937d885b81124569751519
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-21 20:30:08 -0300

    refactor: change userDetailDTO to userDTO

--- src/main/java/com/mlcdev/realestate/mapper/UserMapper.java
+++ src/main/java/com/mlcdev/realestate/mapper/UserMapper.java
@@ -1,6 +1,6 @@
 package com.mlcdev.realestate.mapper;
 
-import com.mlcdev.realestate.dto.UserDetailDTO;
+import com.mlcdev.realestate.dto.UserDTO;
 import com.mlcdev.realestate.entities.User;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -10,8 +10,8 @@ public class UserMapper {
 
     private UserMapper(){}
 
-    public static UserDetailDTO entityToDetailDTO(User entity){
-        return UserDetailDTO.builder()
+    public static UserDTO entityToDTO(User entity){
+        return UserDTO.builder()
                 .id(entity.getId())
                 .username(entity.getUsername())
                 .authorities(entity.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()))
