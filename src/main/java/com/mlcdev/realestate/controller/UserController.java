3530199a6f278af1ff0400c0e1e377c614b5b067
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-21 20:36:37 -0300

    feat: implement the methods and endpoint that let ADMIN create users

--- src/main/java/com/mlcdev/realestate/controller/UserController.java
+++ src/main/java/com/mlcdev/realestate/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.mlcdev.realestate.controller;
 
-import com.mlcdev.realestate.dto.UserDetailDTO;
+import com.mlcdev.realestate.dto.UserCreateDTO;
+import com.mlcdev.realestate.dto.UserDTO;
 import com.mlcdev.realestate.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,15 +24,25 @@ public class UserController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(value = "/{userId}")
-    public ResponseEntity<UserDetailDTO> findById(@PathVariable UUID userId){
-        UserDetailDTO dto = userService.findById(userId);
+    public ResponseEntity<UserDTO> findById(@PathVariable UUID userId){
+        UserDTO dto = userService.findById(userId);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
-    public ResponseEntity<Page<UserDetailDTO>> findAll(@ParameterObject @PageableDefault(page = 0, size = 10) Pageable pageable){
-        Page<UserDetailDTO> dtoPage = userService.findAll(pageable);
+    public ResponseEntity<Page<UserDTO>> findAll(@ParameterObject @PageableDefault(page = 0, size = 10) Pageable pageable){
+        Page<UserDTO> dtoPage = userService.findAll(pageable);
         return ResponseEntity.ok(dtoPage);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody @Valid UserCreateDTO createDTO){
+       UserDTO dto = userService.create(createDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- src/main/java/com/mlcdev/realestate/service/UserService.java
+++ src/main/java/com/mlcdev/realestate/service/UserService.java
@@ -1,13 +1,17 @@
 package com.mlcdev.realestate.service;
 
-import com.mlcdev.realestate.dto.UserDetailDTO;
+import com.mlcdev.realestate.dto.UserCreateDTO;
+import com.mlcdev.realestate.dto.UserDTO;
+import com.mlcdev.realestate.entities.Role;
 import com.mlcdev.realestate.entities.User;
+import com.mlcdev.realestate.exception.ConflictException;
 import com.mlcdev.realestate.exception.NotFoundException;
 import com.mlcdev.realestate.mapper.UserMapper;
 import com.mlcdev.realestate.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,17 +21,34 @@ import java.util.UUID;
 @Service
 public class UserService {
 
+    private final BCryptPasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public UserDetailDTO findById(UUID userId){
+    public UserDTO findById(UUID userId){
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User with Id: " + userId + " not found"));
-        return UserMapper.entityToDetailDTO(user);
+        return UserMapper.entityToDTO(user);
     }
 
     @Transactional(readOnly = true)
-    public Page<UserDetailDTO> findAll(Pageable pageable){
+    public Page<UserDTO> findAll(Pageable pageable){
         Page<User> userPage = userRepository.findAll(pageable);
-        return userPage.map(UserMapper::entityToDetailDTO);
+        return userPage.map(UserMapper::entityToDTO);
     }
+
+    @Transactional
+    public UserDTO create(UserCreateDTO createDTO){
+         if(userRepository.existsByUsername(createDTO.getUsername())){
+             throw new ConflictException("Username not available");
+         }
+         User user = User.builder()
+                 .username(createDTO.getUsername())
+                 .password(passwordEncoder.encode(createDTO.getPassword()))
+                 .build();
+
+         user.addRole(Role.ROLE_BROKER);
+
+         return UserMapper.entityToDTO(userRepository.save(user));
+    }
+
 }
