cba8acadebcdd480a129e866064da132bbe6faff
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-04-24 13:25:29 -0300

    feat: add property broker or admin validation to modify the property images

--- src/main/java/com/mlcdev/realestate/controller/ImageController.java
+++ src/main/java/com/mlcdev/realestate/controller/ImageController.java
@@ -1,11 +1,14 @@
 package com.mlcdev.realestate.controller;
 
 import com.mlcdev.realestate.dto.ImageDTO;
+import com.mlcdev.realestate.security.JwtUtils;
 import com.mlcdev.realestate.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -35,23 +38,23 @@ public class ImageController {
 
     @PreAuthorize("hasRole('BROKER')")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<ImageDTO>> postImages(@PathVariable UUID propertyId, @RequestPart("files") List<MultipartFile> files){
-        List<ImageDTO> imageDTOList = imageService.saveImages(propertyId, files);
+    public ResponseEntity<List<ImageDTO>> postImages(@PathVariable UUID propertyId, @RequestPart("files") List<MultipartFile> files, @AuthenticationPrincipal Jwt jwt){
+        List<ImageDTO> imageDTOList = imageService.saveImages(propertyId, files, JwtUtils.getUserId(jwt), JwtUtils.isAdmin(jwt));
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(propertyId).toUri();
         return ResponseEntity.created(uri).body(imageDTOList);
     }
 
     @PreAuthorize("hasRole('BROKER')")
     @PatchMapping(value = "/{imageId}/primary")
-    public ResponseEntity<ImageDTO> updatePrimaryImage(@PathVariable UUID propertyId, @PathVariable UUID imageId){
-       ImageDTO imageDTO = imageService.updateImageAsPrimary(propertyId, imageId);
+    public ResponseEntity<ImageDTO> updatePrimaryImage(@PathVariable UUID propertyId, @PathVariable UUID imageId, @AuthenticationPrincipal Jwt jwt){
+       ImageDTO imageDTO = imageService.updateImageAsPrimary(propertyId, imageId, JwtUtils.getUserId(jwt), JwtUtils.isAdmin(jwt));
        return ResponseEntity.ok(imageDTO);
     }
 
     @PreAuthorize("hasRole('BROKER')")
     @DeleteMapping(value = "/{imageId}")
-    public ResponseEntity<Void> deleteImage(@PathVariable UUID propertyId, @PathVariable UUID imageId){
-        imageService.deleteImage(propertyId, imageId);
+    public ResponseEntity<Void> deleteImage(@PathVariable UUID propertyId, @PathVariable UUID imageId, @AuthenticationPrincipal Jwt jwt){
+        imageService.deleteImage(propertyId, imageId, JwtUtils.getUserId(jwt), JwtUtils.isAdmin(jwt));
         return ResponseEntity.noContent().build();
     }
 
